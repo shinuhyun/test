d7feeef7541f7cd5f0ff22950de310c82196372b
shinuhyun <shinuhyun@gmail.com>
2024-11-27 14:01:29 +0900

    product

---
+++ src/mock/product/test/product_service_no_di.test.js
@@ -0,0 +1,28 @@
+const ProductClient = require("../product_client.js");
+const ProductService = require("../product_service_no_di.js");
+jest.mock("../product_client");
+
+describe("ProductService", () => {
+  const fetchItems = jest.fn(async () => [
+    { item: "🥛", available: true },
+    { item: "🍌", available: false },
+  ]);
+
+  ProductClient.mockImplementation(() => {
+    return {
+      fetchItems,
+    };
+  });
+
+  let productService;
+
+  beforeEach(() => {
+    productService = new ProductService();
+  });
+
+  it("should filter out only available items", async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(items.length).toBe(1);
+    expect(items).toEqual([{ item: "🥛", available: true }]);
+  });
+});
